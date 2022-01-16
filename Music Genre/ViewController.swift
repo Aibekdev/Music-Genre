6a2db5df4fc9bde61a18f0b00753b750799f028d
Aibekbjj <aibekon@mail.ru>
2022-01-16 19:32:08 +0600

    Initial Commit

---
+++ Music Genre/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music Genre
+//
+//  Created by Aibek on 16.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
