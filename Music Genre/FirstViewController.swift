afec688476f13514ee6bfc909feb7346331b58b4
Aibekbjj <aibekon@mail.ru>
2022-02-14 16:06:04 +0600

    finished project

---
+++ Music Genre/FirstViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FirstViewController.swift
+//  Music Genre
+//
+//  Created by Aibek on 14.02.2022.
+//
+
+import Foundation
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    
+        // Do any additional setup after loading the view.
+        
+    @IBOutlet weak var musicGenre: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        musicGenre.text = ""
+        var charIndex = 0.0
+        let titleText = "Music Genre"
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+                self.musicGenre.text?.append(letter)
+            }
+            charIndex += 1
+        }
+    }
+}
+
--- Music Genre/ViewController.swift
+++ Music Genre/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    
+    @IBAction func musicPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+        
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+        
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+    }
+    
+        func playSound(soundName: String) {
+            let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+            
+        }
+        
+    }
 }
-
